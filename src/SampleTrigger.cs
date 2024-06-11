a6b60b31ab06f6326f194401d867db080f776b2c
Arash Jalalat <a.jalalat@gmail.com>
2024-06-11 20:35:41 +0200

    add final changes

--- src/SampleTrigger.cs
+++ src/SampleTrigger.cs
@@ -18,7 +18,7 @@ namespace LocalFunctionProj
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions Arash!!!!!!!!");
+            return new OkObjectResult("Welcome to Azure Functions running on Container Apps!");
         }
     }
 }
